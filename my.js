0937eb539791aa5a44a95d52a6e63d50d0c18889
seonghwan <hoanshim01@handong.ac.kr>
2023-10-08 16:18:09 +0900

    update

---
+++ my.js
@@ -0,0 +1,39 @@
+function adding(){
+    var name = document.addForm.name.value;
+    var age = document.addForm.age.value;
+    var phone = document.addForm.phone.value;
+    var workPlace = document.addForm.workPlace.value;
+    var relation = document.addForm.relation.value;
+
+    if(name ==''){
+        alert("이름을 입력하세요!!!");
+        name.focus();
+        return 0;
+    }
+
+    if(age ==''){
+        alert("나이를 입력하세요!!!");
+        age.focus();
+        return 0;
+    }
+
+    if(phone ==''){
+        alert("휴대폰 번호를 입력하세요!!!");
+        phone.focus();
+        return 0;
+    }
+
+    if(workPlace ==''){
+        alert("직장을 입력하세요!!!");
+        workPlace.focus();
+        return 0;
+    }
+
+    if(relation ==''){
+        alert("관계를 입력하세요!!!");
+        relation.focus();
+        return 0;
+    }
+
+    alert("연락처가 추가되었습니다.");
+}
